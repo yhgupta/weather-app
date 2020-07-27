0a4e34ec962a025939b30f26ef96bfc1603e02db
Yash Gupta <yhgupta0408@gmail.com>
2020-07-27 22:29:02 +0530

    for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,8 +11,7 @@ const messageTwo = document.querySelector('#message-2')
 weatherform.addEventListener('submit' , (e)=>{
     e.preventDefault()
     const location = search.value
-    const url = "http://localhost:3000/weather?address=" + location
-    fetch(url).then((response) => {
+    fetch("/weather?address=" + location).then((response) => {
         response.json().then((data) => {
             if( data.error ){
                 messageOne.textContent = data.error
